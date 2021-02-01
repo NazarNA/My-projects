74a36cd1c179b4086d3a397354d913929be2a8b2
Nazar <nazarnovosad610@gmail.com>
2021-02-01 23:53:33 +0200

    update

--- React-JS/weatherApp/open-weather-map/src/App.js
+++ React-JS/weatherApp/open-weather-map/src/App.js
@@ -16,9 +16,9 @@ const App = ({ loading, icon, temp, humidity, description, country, city, fetchW
   if(loading) return <div>Loading...</div>
   
   return (
-    <div className='container-fluid'>
-      <ul className="list-group list-group-flush col-md-4 border border-primary m-5">
-        <li className="list-group-item d-flex flex-row justify-content-evenly">
+    <div className='container-md mt-2 d-flex flex-column '>
+      <ul className="list-group list-group-flush col-md-4 border border-primary">
+        <li className="list-group-item d-flex flex-row justify-content-between">
           <div className='d-flex align-items-center'>
             {formater(description)}
           </div>
@@ -26,23 +26,23 @@ const App = ({ loading, icon, temp, humidity, description, country, city, fetchW
             <img src={`https://openweathermap.org/img/wn/${icon}.png`} />
           </div>
         </li>
-        <li className="list-group-item d-flex flex-row justify-content-evenly">
+        <li className="list-group-item d-flex flex-row justify-content-between">
           <div>
-            <img className="img-fluid" style={{width: 25 + 'px'}} src='https://www.freepngimg.com/thumb/map/62759-computer-location-icon-icons-free-download-png-hd.png'/> 
+            <img className="img-fluid" style={{width: 25 + 'px'}} src='https://uxwing.com/wp-content/themes/uxwing/download/21-maps-and-location/find-location.png'/> 
           </div>
           <div className='d-flex align-items-center'>
             {formater(city)} {country && country}
           </div>
         </li>
-        <li className="list-group-item d-flex flex-row justify-content-evenly">
+        <li className="list-group-item d-flex flex-row justify-content-between">
           <div className='d-flex flex-row'>
-            <img className="img-fluid" style={{width: 35 + 'px'}} src='https://i.pinimg.com/originals/b2/d6/c2/b2d6c24a4a7f3f0529c2efdaeff54bc5.png'/> 
+            <img className="img-fluid" style={{width: 35 + 'px'}} src='https://cdn0.iconfinder.com/data/icons/auto-workshop/50/51-512.png'/> 
           </div>
           <div className='d-flex align-items-center'>
             {parseInt(temp) - 273}&deg;С
           </div>
         </li>
-        <li className="list-group-item d-flex flex-row justify-content-evenly">
+        <li className="list-group-item d-flex flex-row justify-content-between">
           <div className='d-flex flex-row'>
             <img className="img-fluid" style={{width: 35 + 'px'}} src='https://image.flaticon.com/icons/png/512/219/219816.png' /> 
           </div>
@@ -51,7 +51,7 @@ const App = ({ loading, icon, temp, humidity, description, country, city, fetchW
           </div>
         </li>
       </ul>
-      <div className="col-md-4 ms-5">
+      <div className="col-md-4 mt-1">
         <input type="text" ref={input} className="form-control col-md-4" placeholder="City..." />
         <button onClick={()=> setCity(input.current.value)} className="btn btn-primary mt-1 col-md-12" type="button">Weather</button>
       </div>
